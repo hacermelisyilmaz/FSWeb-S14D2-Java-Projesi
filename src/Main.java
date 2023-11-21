40f6414ce7b9056daa72ee0969c4855ee02be619
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-21 15:48:38 +0300

    Bedroom object is created in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bedroom bedroom = new Bedroom("Master bedroom",
+                new Wall("north"), new Wall("east"), new Wall("south"), new Wall("west"),
+                new Ceiling(3, PaintColor.BLUE),
+                new Bed("King", 2, 3, 2, 1),
+                new Lamp(LampType.BEDSIDE, true, 10),
+                new Wardrobe(1, 2, 10),
+                new Carpet(2, 2, PaintColor.WHITE));
+
     }
 }
