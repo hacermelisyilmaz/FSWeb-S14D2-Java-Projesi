0477817bf15f39c3c6567938c5cd2e698566286c
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-21 15:30:54 +0300

    Ceiling: Constructor is set

--- src/Ceiling.java
+++ src/Ceiling.java
@@ -1,4 +1,9 @@
 public class Ceiling {
     private int height;
     private PaintColor paintedColor;
+
+    public Ceiling(int height, PaintColor paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
 }
