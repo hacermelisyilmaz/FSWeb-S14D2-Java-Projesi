51987e219cd742d59a1deab6c734c616353b94cd
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-21 15:27:57 +0300

    Carpet: Constructor is set

--- src/Carpet.java
+++ src/Carpet.java
@@ -2,4 +2,10 @@ public class Carpet {
     private int width;
     private int height;
     PaintColor color;
+
+    public Carpet(int width, int height, PaintColor color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
 }
