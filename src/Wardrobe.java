0323c469dd90ee1772327c941680ca67bd2da506
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-21 15:22:23 +0300

    Wardrobe: Constructor is set

--- src/Wardrobe.java
+++ src/Wardrobe.java
@@ -2,4 +2,10 @@ public class Wardrobe {
     private int width;
     private int height;
     private double weight;
+
+    public Wardrobe(int width, int height, double weight) {
+        this.width = width;
+        this.height = height;
+        this.weight = weight;
+    }
 }
